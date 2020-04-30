4051f9acefa7b79c91d846c8de10aa68ce29594b
Suzann Kowalski <suzannaudra@gmail.com>
2020-04-29 18:02:34 -0700

    working app

--- models/burger.js
+++ models/burger.js
@@ -6,14 +6,14 @@ var burger = {
       cb(res);
     })
   }
-}
+,
 update: function(id,cb){
   orm.update('burgers',id,cb);
 }
-
+,
 create: function(name,cb){
   orm.create('burgers',name,cb);
 }
-
+}
 
 module.exports = burger;
